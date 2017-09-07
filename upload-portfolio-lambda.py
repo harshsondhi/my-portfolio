21ee7650116410279b6c45f31e02929ca4c373c5
harshsondhi <harshsondhi2008@gmail.com>
2017-09-07 16:10:37 -0700

    added lambda script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,14 @@
+import boto3
+import mimetypes
+s3 = boto3.resource('s3')
+portfolio_bucket = s3.Bucket('portfolio.harshsondhi.info')
+build_bucket=s3.Bucket('portfoliobuild.harshsondhi.info')
+import StringIO
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+import zipfile
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj=myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj,nm)
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
