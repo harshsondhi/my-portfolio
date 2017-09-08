280a4abdba381e7e99e7234b47f6bf9937e0d5b0
harshsondhi <harshsondhi2008@gmail.com>
2017-09-07 17:47:15 -0700

    adding updated lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,14 +1,17 @@
 import boto3
-import mimetypes
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('portfolio.harshsondhi.info')
-build_bucket=s3.Bucket('portfoliobuild.harshsondhi.info')
 import StringIO
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
 import zipfile
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj=myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj,nm)
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    portfolio_bucket = s3.Bucket('portfolio.harshsondhi.info')
+    build_bucket=s3.Bucket('portfoliobuild.harshsondhi.info')
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj=myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    print "Job Done!!!!"
+    return 'Hello from Lambda'
